7d891f101d27f79345e4e7e1ec16447f3c0f11d8
chunhua.zhang <chunhua.zhang@gmail.com>
2021-03-01 11:31:33 +0900

    First redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CounterContainer from './containers/CounterContainer';
-import './assets/styles/main.css';
+import * as React from 'react'
+class AppProps {
+    propsMessage: string
+}
+const AppComponent = (props: AppProps) => <p>{props.propsMessage}</p>
 
+// redux boilerplate
+import { createStore } from 'redux'
+import { connect } from 'react-redux'
+class StoreState {
+    storeMessage: string
+}
+const mapStateToProps = (state: StoreState) => ({ propsMessage: state.storeMessage })
+const AppContainer = connect(mapStateToProps)(AppComponent)
+const reducer = () => ({ storeMessage: 'Hello world' })
+const store = createStore(reducer)
 
+// Render the app
+import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 ReactDOM.render(
-  <CounterContainer />,
-    document.getElementById("render-app")
-);
+    <Provider store={store}><AppContainer /></Provider>,
+    document.getElementById('render-app')
+)
